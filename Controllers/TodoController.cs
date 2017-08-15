a9298c247176495c529373dfaf137bb3ea20b7d0
allengblack <allengblack@gmail.com>
2017-08-15 18:10:18 +0100

    add POST method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -20,5 +20,36 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id}, item);
+        }
     }
 }
